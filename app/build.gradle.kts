b0869b165deebff0f1b1e42ec13c70f8f4cddba2
mahdiasdn <mahdiasadyan@gmail.com>
2026-08-13 09:27:10 +0000

    v1.2.4 - stability and bug fixes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.iliyateam.aseman"
         minSdk = 26
         targetSdk = 35
-        versionCode = 6
-        versionName = "1.2.3"
+        versionCode = 7
+        versionName = "1.2.4"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
